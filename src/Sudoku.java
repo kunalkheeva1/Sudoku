43b3107ec71af34d6c2aa0ecf9f04abf3abb90b3
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-04 21:08:49 -0700

    issued a board

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,5 +1,6 @@
 public class Sudoku {
-    public static final int SIZE_OF_GRID = 9;  
+    public static final int SIZE_OF_GRID = 9;  //creating a size of the grid variable for easy use in next steps
+    // now issuing my own board with vacant spots as zeros in it
 
     int [][] board = {{7,2,0,8,0,0,0,9,0},
                       {6,4,9,0,0,7,0,5,0},
@@ -11,6 +12,7 @@ public class Sudoku {
                       {0,0,0,0,0,0,0,1,0},
                       {0,0,0,0,8,1,0,0,0}};
 
+    //creating a method to search in the row
     public static boolean isNumberInRow(int [][]board, int number, int row){
 
 
