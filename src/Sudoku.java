1e3a603ffa4fbced6567f4f3c6abeb9c4c01d2bd
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-04 21:06:13 -0700

    Initial commit

---
+++ src/Sudoku.java
@@ -0,0 +1,22 @@
+public class Sudoku {
+    public static final int SIZE_OF_GRID = 9;  
+
+    int [][] board = {{7,2,0,8,0,0,0,9,0},
+                      {6,4,9,0,0,7,0,5,0},
+                      {5,0,8,2,9,4,0,0,7},
+                      {0,7,6,0,0,0,3,0,1},
+                      {0,0,4,9,0,3,0,7,0},
+                      {0,5,2,1,0,0,4,6,0},
+                      {0,0,1,6,3,5,7,0,0},
+                      {0,0,0,0,0,0,0,1,0},
+                      {0,0,0,0,8,1,0,0,0}};
+
+    public static boolean isNumberInRow(int [][]board, int number, int row){
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
