cda88133d730b6ea9ad61852f5b1d4014baf63c0
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-04 22:57:02 -0700

    created a print method to print the solved game with clarity

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,16 +1,41 @@
+import javax.swing.*;
+
 public class Sudoku {
     public static final int SIZE_OF_GRID = 9;  //creating a size of the grid variable for easy use in next steps
     // now issuing my own board with vacant spots as zeros in it
 
-    int [][] board = {{7,2,0,8,0,0,0,9,0},
-                      {6,4,9,0,0,7,0,5,0},
-                      {5,0,8,2,9,4,0,0,7},
-                      {0,7,6,0,0,0,3,0,1},
-                      {0,0,4,9,0,3,0,7,0},
-                      {0,5,2,1,0,0,4,6,0},
-                      {0,0,1,6,3,5,7,0,0},
-                      {0,0,0,0,0,0,0,1,0},
-                      {0,0,0,0,8,1,0,0,0}};
+    public static void main(String[] args) {
+
+        int[][] board = {{7, 2, 0, 8, 0, 0, 0, 9, 0},
+                {6, 4, 9, 0, 0, 7, 0, 5, 0},
+                {5, 0, 8, 2, 9, 4, 0, 0, 7},
+                {0, 7, 6, 0, 0, 0, 3, 0, 1},
+                {0, 0, 4, 9, 0, 3, 0, 7, 0},
+                {0, 5, 2, 1, 0, 0, 4, 6, 0},
+                {0, 0, 1, 6, 3, 5, 7, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 0},
+                {0, 0, 0, 0, 8, 1, 0, 0, 0}};
+        printBoard(board);
+
+    }
+
+    //creating a method to print board
+    public static void printBoard(int[][]board){
+        for(int row=0; row<SIZE_OF_GRID; row++){
+            if(row%3==0&& row!=0){
+                System.out.println("_________________");    //just providing a lines after three upper grids
+            }
+            for (int column=0; column<SIZE_OF_GRID; column++){
+                if(column%3==0 && column!=0){
+                    System.out.println(" | ");              //after three columns printing this line for clarity
+                }
+                System.out.print(board[row][column]+ " ");
+            }
+            System.out.println();
+        }
+    }
+
+
 
     //creating a method to search in the row
     public static boolean isNumberInRow(int [][]board, int number, int row){
@@ -80,7 +105,5 @@ public class Sudoku {
 
     }
 
-    public static void main(String[] args) {
 
-    }
 }
